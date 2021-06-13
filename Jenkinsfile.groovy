dce7ab0499cab41fe4c4fde3771215d2479f4298
Armand Gaillard <armandgaillard@Armands-MacBook-Air.local>
2021-06-13 23:11:21 +0100

    add go installation details to jenkins file

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,63 +1,60 @@
 // Run on an agent where we want to use Go
+
 node {
-    // Ensure the desired Go version is installed
-    def root = tool type: 'go', name: 'Go 1.16.4'
+    def gitRepository = 'https://github.com/agdevtools/go-postgres.git/'
+    def gitBranch = '*/master'
+    def githubcreds = [
+            $class      : 'UsernamePasswordMultiBinding',
+            credentialsId : 'githubcreds',
+            usernameVariable : 'GIT_USER',
+            passwordVariable : 'GIT_PASS'
+    ]
 
-    // Export environment variables pointing to the directory where Go was installed
-    withEnv(["GOROOT=${root}", "PATH+GO=${root}/bin"]) {
-        sh 'go version'
+    stage('Clean Workspace') {
+        cleanWs()
     }
 
+    stage('Git Checkout') {
+        withCredentials([githubcreds]){
+            checkout([
+                    $class      : 'GitSCM',
+                    branches    : [[name:"${gitBranch}"]],
+                    doGenerateSubModuleConfigurations : false,
+                    extensions: [],
+                    submoduleCfg: [],
+                    userRemoteConfigs: [[credentialsId  : 'githubcreds',
+                                         url            :"${gitRepository}"]]
 
-    node {
-        def gitRepository = 'https://github.com/agdevtools/go-postgres.git/'
-        def gitBranch = '*/master'
-        def githubcreds = [
-                $class          : 'UsernamePasswordMultiBinding',
-                credentialsId   : 'githubcreds',
-                usernameVariable: 'GIT_USER',
-                passwordVariable: 'GIT_PASS'
-        ]
+            ])
 
-        stage('Clean Workspace') {
-            cleanWs()
         }
 
-        stage('Git Checkout') {
-            withCredentials([githubcreds]) {
-                checkout([
-                        $class                           : 'GitSCM',
-                        branches                         : [[name: "${gitBranch}"]],
-                        doGenerateSubModuleConfigurations: false,
-                        extensions                       : [],
-                        submoduleCfg                     : [],
-                        userRemoteConfigs                : [[credentialsId: 'githubcreds',
-                                                             url          : "${gitRepository}"]]
+        // Ensure the desired Go version is installed
+        def root = tool type: 'go', name: 'Go 1.16.4'
 
-                ])
-
-            }
-        }
-
-        stage('Build') {
+        // Export environment variables pointing to the directory where Go was installed
+        withEnv(["GOROOT=${root}", "PATH+GO=${root}/bin"]) {
             sh 'go version'
-            sh 'go get -u golang.org/x/lint/golint'
-            sh 'go build -o bin/main main/main.go'
-
-        }
-
-        stage('Test') {
-
-            sh 'echo running tests ...'
-
-        }
-
-        stage('Deploy to Heroku') {
-
-            sh 'git commit --allow-empty -m "Trigger Heroku deploy after enabling collectstatic"'
-            sh 'git push'
-
         }
     }
 
+    stage('Build') {
+        sh 'go version'
+        sh 'go get -u golang.org/x/lint/golint'
+        sh 'go build -o bin/main main/main.go'
+
+    }
+
+    stage('Test') {
+
+        sh 'echo running tests ...'
+
+    }
+
+    stage('Deploy to Heroku') {
+
+        sh 'git commit --allow-empty -m "Trigger Heroku deploy after enabling collectstatic"'
+        sh 'git push'
+
+    }
 }
