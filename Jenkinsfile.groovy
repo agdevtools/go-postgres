ff90017632ff8c1b756d32a24f5178d57143003f
Armand Gaillard <armandgaillard@Armands-MacBook-Air.local>
2021-06-13 23:06:57 +0100

    add go installation details to jenkins file

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -7,57 +7,57 @@ node {
     withEnv(["GOROOT=${root}", "PATH+GO=${root}/bin"]) {
         sh 'go version'
     }
-}
 
 
+    node {
+        def gitRepository = 'https://github.com/agdevtools/go-postgres.git/'
+        def gitBranch = '*/master'
+        def githubcreds = [
+                $class          : 'UsernamePasswordMultiBinding',
+                credentialsId   : 'githubcreds',
+                usernameVariable: 'GIT_USER',
+                passwordVariable: 'GIT_PASS'
+        ]
 
-node {
-    def gitRepository = 'https://github.com/agdevtools/go-postgres.git/'
-    def gitBranch = '*/master'
-    def githubcreds = [
-            $class      : 'UsernamePasswordMultiBinding',
-            credentialsId : 'githubcreds',
-            usernameVariable : 'GIT_USER',
-            passwordVariable : 'GIT_PASS'
-    ]
+        stage('Clean Workspace') {
+            cleanWs()
+        }
 
-    stage('Clean Workspace') {
-        cleanWs()
-    }
+        stage('Git Checkout') {
+            withCredentials([githubcreds]) {
+                checkout([
+                        $class                           : 'GitSCM',
+                        branches                         : [[name: "${gitBranch}"]],
+                        doGenerateSubModuleConfigurations: false,
+                        extensions                       : [],
+                        submoduleCfg                     : [],
+                        userRemoteConfigs                : [[credentialsId: 'githubcreds',
+                                                             url          : "${gitRepository}"]]
 
-    stage('Git Checkout') {
-        withCredentials([githubcreds]){
-            checkout([
-                    $class      : 'GitSCM',
-                    branches    : [[name:"${gitBranch}"]],
-                    doGenerateSubModuleConfigurations : false,
-                    extensions: [],
-                    submoduleCfg: [],
-                    userRemoteConfigs: [[credentialsId  : 'githubcreds',
-                                         url            :"${gitRepository}"]]
+                ])
 
-            ])
+            }
+        }
+
+        stage('Build') {
+            sh 'go version'
+            sh 'go get -u golang.org/x/lint/golint'
+            sh 'go build -o bin/main main/main.go'
+
+        }
+
+        stage('Test') {
+
+            sh 'echo running tests ...'
+
+        }
+
+        stage('Deploy to Heroku') {
+
+            sh 'git commit --allow-empty -m "Trigger Heroku deploy after enabling collectstatic"'
+            sh 'git push'
 
         }
     }
 
-    stage('Build') {
-        sh 'go version'
-        sh 'go get -u golang.org/x/lint/golint'
-        sh 'go build -o bin/main main/main.go'
-
-    }
-
-    stage('Test') {
-
-        sh 'echo running tests ...'
-
-    }
-
-    stage('Deploy to Heroku') {
-
-        sh 'git commit --allow-empty -m "Trigger Heroku deploy after enabling collectstatic"'
-        sh 'git push'
-
-    }
 }
